b675caf97e25ddc13cc11fb795ce95e1db0e0349
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-11-08 10:38:01 +0100

    Načtení dat, logika pro extrakci požadovaných dat z json souboru - name, created_at, status, cpu_usage, memory_usage, ip_addresses a uložení do seznamu slovníků

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+# Načíst data
+
+with open("sample-data.json", "r") as file:
+    data = json.load(file)
+
+# vytvořit seznam, který bude obsahovat slovník s požadovanými daty k danému kontejneru
+container_list = []
+
+for container in data:
+    container_dict = {}
+    container_dict["name"] = container["name"]
+    container_dict["created_at"] = container["created_at"]
+    container_dict["status"] = container["status"]
+
+    ip_addresses = []
+    if container["state"] is None:
+        container_dict["cpu_usage"] = None
+        container_dict["memory_usage"] = None
+    else:
+        container_dict["cpu_usage"] = container["state"]["cpu"]["usage"]
+        container_dict["memory_usage"] = container["state"]["memory"]["usage"]
+
+        for device_name, device_details in container["state"]["network"].items():
+            for data in device_details["addresses"]:
+                ip_addresses.append(data["address"])
+
+    container_dict["ip_addresses"] = ip_addresses
+    container_list.append(container_dict)
+
+for container in container_list:
+    print(container)
