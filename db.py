41f96e249941b02d7efa515d769eac1b156b6bf6
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-11-09 13:31:59 +0100

    v db.py jsem přidal engine a async_session_factory a přidal funkci get_db, která vytvoří novou async session pro každý požadavek

--- db.py
+++ db.py
@@ -1,11 +1,20 @@
-from dotenv import load_dotenv
 import os
-
-# Budu potřebovat nastavit DATABASE_URL
-# Budu potřebovat nastavit engine
-# Budu potřebovat nastavit session
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
+from models import Base
 
 load_dotenv()
+
+# Budu potřebovat nastavit DATABASE_URL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Budu potřebovat nastavit engine
+engine = create_async_engine(DATABASE_URL, echo=True)
 
+# Budu potřebovat nastavit session
+async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db() -> AsyncSession:
+    async with async_session_factory() as session:
+        yield session
