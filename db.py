48bd98731687918dd836b1299e166e149499cadc
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-11-09 12:30:39 +0100

    Nastavil jsem asynchronní připojení k databázi přes DATABASE_URL, kterou načítám z .env souboru pomocí python-dotenv v db.py. Zároveň jsem vytvořil .env.example jako šablonu.

---
+++ db.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+# Budu potřebovat nastavit DATABASE_URL
+# Budu potřebovat nastavit engine
+# Budu potřebovat nastavit session
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
